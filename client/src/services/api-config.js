182c7ca8b2658098a39f167188807e7d77d7d03e
Tyler Huyser <tyler.huyser@gmail.com>
2021-09-13 23:56:06 -0400

    added new api copnfig baseurl

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? `https://www.gameset.blog/wp-json/wp/v2` : 'https://www.gameset.blog/wp-json/wp/v2'
+const baseUrl = process.env.NODE_ENV === 'production' ? `https://www.gamesetblog.com/wp-json/wp/v2` : 'https://www.gamesetblog.com/wp-json/wp/v2'
 
 const api = axios.create({
   url: baseUrl

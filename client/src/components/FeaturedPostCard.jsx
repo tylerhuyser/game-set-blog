3506b3f0f4c7fe50bd274f9c7f6621561b416af7
Tyler Huyser <tyler.huyser@gmail.com>
2023-07-18 20:50:54 -0400

    renamed featuredcard css sheet and deleted old one.

--- client/src/components/FeaturedPostCard.jsx
+++ client/src/components/FeaturedPostCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 
-import './FeaturedPostCard1.css'
+import './FeaturedPostCard.css'
 
 export default function FeaturedPostCard(props) {
 
@@ -26,28 +26,6 @@ export default function FeaturedPostCard(props) {
       {postData && users ?
 
         <>
-          {/* <div className="featuredPostCard-container" key={postData.id} onClick={handlePost} >
-
-            <div className="post-content">
-
-                <p className="post-date">{`${postMonth}.${postDate}.${postYear}`}</p>
-
-                <p className="post-title">{parse(postData.title.rendered).toUpperCase()}</p>
-
-              <div className="post-excerpt">{parse(postData.excerpt.rendered.slice(0, 250).slice(0, postData.excerpt.rendered.slice(0, 250).lastIndexOf(".")).trim("Continue reading").concat("", "."))}</div>
-
-              <p onClick={handlePost} className="post-link">Read Full Article...</p>
-
-            </div>
-
-            <div className="post-image">
-              <a href="">
-                  <img className="img" src={postData["_embedded"]["wp:featuredmedia"][0].source_url} alt="featured-postCard-image" />
-                  
-              </a>
-            </div>
-            
-          </div> */}
 
           <div className="featured-post-card-container" key={postData.id} onClick={handlePost} >
 
@@ -71,12 +49,9 @@ export default function FeaturedPostCard(props) {
             </div>
 
             <div className='featured-post-card-image-container'>
-            {/* <div className="post-image">
-              <a href=""> */}
+
                   <img className="featured-post-card-image" src={postData["_embedded"]["wp:featuredmedia"][0].source_url} alt="featured-postCard-image" />
-                  {/* <Img fluid={postData["_embedded"]["wp:featuredmedia"][0].source_url} alt={parse(postData.title.rendered)} className="img"/> */}
-              {/* </a>
-            </div> */}
+  
             </div>
             
           </div>

3c519c000b8129e0dab388f201c02d527260df66
Tyler Huyser <tyler.huyser@gmail.com>
2023-11-12 17:55:20 -0500

    removed unused npm pacakges from package-json."
    "

--- client/src/components/shared/Header.jsx
+++ client/src/components/shared/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { loaderDelay } from '../../utils';
 import { useScrollDirection } from '../../hooks';
@@ -145,7 +144,3 @@ export default function Header(props) {
     </>
   )
 }
-
-Header.propTypes = {
-  isHome: PropTypes.bool,
-};

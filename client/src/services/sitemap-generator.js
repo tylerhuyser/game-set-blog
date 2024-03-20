6fe8a367059f2543bcfdfc0e895447cad7947852
Tyler Huyser <tyler.huyser@gmail.com>
2024-03-20 15:59:38 -0400

    removed tags and categories from sitemap.

--- client/src/services/sitemap-generator.js
+++ client/src/services/sitemap-generator.js
@@ -31,44 +31,20 @@ const getPosts = async () => {
   return resp.data
 }
 
-const getCategories = async () => {
-  const resp = await api.get(`/categories?_embed&per_page=100`)
-  return resp.data
-}
-
-const getTags = async () => {
-  const resp = await api.get(`/tags?_embed&per_page=100`)
-  return resp.data
-}
-
 
 async function generateSitemap() {
   try {
     const postsData = await getPosts()
-    const categoriesData = await getCategories()
-    const tagsData = await getTags()
 
     let posts = []
-    let categories = []
-    let tags = []
 
 
     for(var i = 0; i < postsData.length; i++) {
       posts.push({ slug: postsData[i].slug });
     }
 
-    for(var i = 0; i < categoriesData.length; i++) {
-      categories.push({ id: categoriesData[i].id, slug: categoriesData[i].slug });
-    }
-
-    for(var i = 0; i < tagsData.length; i++) {
-      tags.push({ id: tagsData[i].id, slug: tagsData[i].slug });
-    }
-
     const paramsConfig = {
       "/posts/:slug": posts,
-      "/categories/:id/:slug": categories,
-      "/tags/:id/:slug": tags
     };
 
     return (
